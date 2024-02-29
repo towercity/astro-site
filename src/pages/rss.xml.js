22b766428b6c4c052aedd8301769c11dea4b971c
Matt Nerger <matthewjnerger3@gmail.com>
2024-02-28 21:41:45 -0500

    rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,19 @@
 import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import sanitizeHtml from 'sanitize-html';
 
 export async function GET(context) {
+    const postImportResult = import.meta.glob('../posts/**/*.md', { eager: true });
+    const posts = Object.values(postImportResult);
+
     return rss({
         title: 'j konger | blog',
         description: 'a nice site',
         site: context.site,
-        items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+        items: posts.map((post) => ({
+            link: post.url,
+            content: sanitizeHtml(post.compiledContent()),
+            ...post.frontmatter,
+        })),
         customData: `<language>en-us</language>`,
     });
 }
