e2deeb1f1ab8513e5115137275f07de2004b7a60
Matt Nerger <matthewjnerger3@gmail.com>
2025-01-24 23:58:11 -0500

    rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,18 +2,25 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 import sanitizeHtml from 'sanitize-html';
 
 export async function GET(context) {
-    const postImportResult = import.meta.glob('../pages/posts/**/*.md', { eager: true });
+    const postImportResult = import.meta.glob('../pages/posts/**/*.*', { eager: true });
     const posts = Object.values(postImportResult);
 
     return rss({
         title: 'j konger | blog',
         description: 'a nice site',
         site: context.site,
-        items: posts.map((post) => ({
-            link: post.url,
-            content: sanitizeHtml(post.compiledContent()),
-            ...post.frontmatter,
-        })),
+        items: posts.map((post) => {
+            const item = {
+                link: post.url,
+                ...post.frontmatter,
+            }
+            if( post.compiledContent ) {
+                item.content = sanitizeHtml(post.compiledContent())
+            } else {
+                item.content = 'this is a special post! to view it please open in a browser'
+            }
+            return item;
+        }),
         customData: `<language>en-us</language>`,
     });
 }
