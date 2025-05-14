a91dfa29dda67da6592b83f2415e07eecbabd61d
Matt Nerger <matthewjnerger3@gmail.com>
2025-05-13 21:51:34 -0400

    Start rng functions

---
+++ src/scripts/rng.js
@@ -0,0 +1,21 @@
+/**
+ * Return a random item from an array.
+ *
+ * @param {array} array  Array to look through
+ * @returns Item at a random index
+ */
+const randomItemFrom = (array) => {
+  return array[Math.floor(random() * array.length)];
+};
+
+/**
+ * Return a random number
+ *
+ * Wrapper so we can do our own stuff later, if wanted
+ *
+ * @param {int} range  High range for random number
+ * @returns A random
+ */
+const random = (range = 1) => {
+  return Math.random() * range;
+};
