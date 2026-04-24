33cebcda840ae3b921e5da98d942812662403ccc
Matt Nerger <mnerger@ra.org>
2026-04-24 13:06:17 -0400

    Try no rss feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,29 +1,27 @@
 import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 
-import sanitizeHtml from 'sanitize-html';
+export async function GET( context ) {
+    const posts = await getCollection( 'blog' );
 
-export async function GET(context) {
-    const posts = await getCollection('blog');
-
-    return rss({
+    return rss( {
         title: 'j konger | blog',
         description: 'a nice website',
         site: context.site,
-        items: posts.map((post) => {
+        items: posts.map( ( post ) => {
             const item = {
-              title: post.data.title,
-              pubDate: post.data.pubDate,
-              link: `/posts/${post.id}`,
-              description: 'post'
+                title: post.data.title,
+                pubDate: post.data.pubDate,
+                link: `/posts/${post.id}`,
+                description: 'post'
             }
-            if( post.rendered.html ) {
-                item.content = post.rendered.html
-            } else {
-                item.content = 'this is a special post! to view it please open in a browser'
-            }
+            // if ( post.rendered.html ) {
+            //     item.content = post.rendered.html
+            // } else {
+            //     item.content = 'this is a special post! to view it please open in a browser'
+            // }
             return item;
-        }),
+        } ),
         customData: `<language>en-us</language>`,
-    });
+    } );
 }
