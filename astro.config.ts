7ed0316006878bcf537d7f359567a9569b4b305c
Matt Nerger <matthewjnerger3@gmail.com>
2026-05-08 16:03:36 -0400

    update some config

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-
 import mdx from "@astrojs/mdx";
+import { rehypeHeadingIds } from '@astrojs/markdown-remark';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://konger.online",
   integrations: [sitemap(), mdx()],
   compressHTML: false,
+  markdown: {
+    rehypePlugins: [rehypeHeadingIds],
+  },
+  scopedStyleStrategy: 'where',
 });
